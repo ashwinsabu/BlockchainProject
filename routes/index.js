a25fdf7e60ef298754f8b0b4ccddb91d98d2f145
ashwin <x23196505@student.ncirl.ie>
2024-03-10 23:03:03 +0000

    front end pages

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+/* GET destinations page. */
+router.get('/destinations', function(req, res, next) {
+  res.render('destinations', { title: 'Express' });
+});
+
+
+module.exports = router;
